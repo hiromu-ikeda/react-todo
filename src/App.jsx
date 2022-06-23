d5d02b569a5d9e24c1c1d2a35bed2fafd139dc88
hiromu-ikeda <hiromu.ikeda@ynot.jp>
2022-06-23 10:12:31 +0900

    addTodo

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,49 @@
+import react, {useState} from "react"
 import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
+  const [todoText, setTodoText] = useState('');
   const [incompleteTodos, setIncompleteTodos] = useState(['ああああ', 'いいいい']);
+  const [completeTodos, setCompleteTodos] = useState(['うううう']);
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText]
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  }
   return (
     <>
-      <div className="inputArea">
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+      <div className="input-area">
+        <input placeholder="TODOを入力" value={todoText} onChange={onChangeTodoText}/>
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のtodo</p>
         <ul>
           {incompleteTodos.map((todo) => {
             return (
-              <div className='list-row'>
+              <div key={todo} className='list-row'>
               <li>{todo}</li>
               <button>完了</button>
               <button>削除</button>
           </div>
             )
           })}
-          
-          <div className='list-row'>
-            <li>いいいい</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
         </ul>
       </div>
       <div className="complete-area">
         <p className='title'>完了のtodo</p>
         <ul>
-          <div className='list-row'>
-            <li>うううう</li>
-            <button>戻す</button>
-          </div>
+          {completeTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            )
+          })}
         </ul>
       </div>
     </>
